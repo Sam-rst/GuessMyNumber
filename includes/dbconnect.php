e2799bcf59fc27da9322a63f923461f1c37fb2d2
Sam-rst <samuel.ressiot@gmail.com>
2024-03-06 00:07:18 +0100

    Test de connexion à la BDD du serveur de prod, échec..

---
+++ includes/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+require_once("config.php");
+
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+// } catch (PDOException $e) {
+//     $_SESSION['message'] = "La connexion à la base de données a échouée";
+//     $_SESSION['messageColor'] = "danger";
+//     header('Location: ../auth/login.php');
+// }
+
