b613703ab39530a3124a87fe521f439694e21cb4
POWLAIR <114736502+POWLAIR@users.noreply.github.com>
2024-03-06 13:49:39 +0100

    [#Validation dans les entrées] Add controle Validation

--- script.js
+++ script.js
@@ -84,3 +84,25 @@ function updatehighscore() {
 btnAgain.addEventListener("click", resetGame);
 
 resetGame();  // Appeler resetGame pour initialiser nbr_guess au début
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const guessInput = document.querySelector('.guess');
+    guessInput.value = '';
+    guessInput.addEventListener('input', enforceIntegerRange);
+
+    function enforceIntegerRange() {
+        let value = parseInt(this.value, 10);
+
+        if (isNaN(value)|| value === 0) {
+            this.value = '';
+        } else if (value < 1) {
+            this.value = 1;
+        } else if (value > 20) {
+            this.value = 20;
+        } else {
+            this.value = value;
+        } 
+    }
+
+});
